368623a465f95d4b4f0b9215540708bbb6de0e87
Brian Mann <brian.mann@inspectall.com>
2014-03-18 11:24:37 -0400

    wrapped eco.compile in a try/catch to prevent gulp from crashing on eco compile error

--- index.js
+++ index.js
@@ -14,22 +14,24 @@ module.exports = function (opt) {
       return callback();
     }
 
+    var output = "";
+
     JSTpath = path.dirname(file.path) + '/' + path.basename(file.path, '.eco');
     var re = new RegExp('.*\/' + opt.basePath + '\/?');  // match basePath + optional path separator
     JSTpath = JSTpath.replace(re, '');
 
     var str = file.contents.toString();
-    output = eco.compile(str);
-    output = 'this.JST["' + JSTpath + '"] = ' + output + '\n';
-    output = "this.JST || (this.JST = {});\n" + output;
-    output = "(function() {\n" + output + "}).call(this);\n";
 
     try {
+      output = eco.compile(str);
+      output = 'this.JST["' + JSTpath + '"] = ' + output + '\n';
+      output = "this.JST || (this.JST = {});\n" + output;
+      output = "(function() {\n" + output + "}).call(this);\n";
+
       file.contents = new Buffer(output);
       file.path = gutil.replaceExtension(file.path, '.js');
-    } catch (err) {
-      err.fileName = file.path;
-      this.emit('error', new gutil.PluginError('gulp-eco', err));
+    } catch(err) {
+      return this.emit('error', new gutil.PluginError('gulp-eco', err, {showStack: true}));
     }
 
     this.push(file);
